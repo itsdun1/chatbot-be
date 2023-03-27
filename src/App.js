9401d19baff3bb76646b22dffdf0e3fdbfcb4420
Aditya Kale <adityakale@Adityas-MacBook-Pro.local>
2023-03-28 04:11:49 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React,{ useState} from 'react';
 import ChatBot from './components/ChatBot';
 
-function App() {
-  const [user, setUser] = useState('');
-  function getVal() {
-    const val = document.querySelector('input').value;
-    console.log(val);
-  }
-  
+function App() {  
   return (
     <div>
-      {!user ? 
-      <input type="text" placeholder="Enter text" onblur={getVal()}></input>
-      : ''}
       <h1>Mitra</h1>
       <ChatBot />
     </div>
